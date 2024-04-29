9ee4ece08b7c715c3a9e5d3a1ba077412de87deb
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-29 20:13:10 +0200

    feat:list emails

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-      return {};
+      return {
+
+        emailArray: [],
+
+      };
+    },
+
+    created () {
+        this.generatorEmails();
+    },
+
+    methods: {
+        generatorEmails: function () {
+            for (i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((generatedMail) => {
+                    this.emailArray.push(generatedMail.data.response);
+                  });
+              }
+          },
     },
     }).mount("#app");
